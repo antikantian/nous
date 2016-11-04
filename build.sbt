62e3992eaeed5f9087d5a44dedb60565c42170ad
antikantian <antikantian@users.noreply.github.com>
2016-11-04 10:55:30 +0200

    ...

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := "nous-parent"
+name := "nous"
 
 version := "0.1.0"
 
--- nous-core/src/main/scala/nous/data/Indexable.scala
+++ nous-core/src/main/scala/nous/data/Indexable.scala
@@ -2,28 +2,15 @@ package nous.data
 
 import language.experimental.macros
 
-import dogs.{ Vector => DVector }
 import imp.summon
-import nous.data.vector.DenseVector
 
 trait Indexable[F[_]] {
-  def apply[A](idx: Int, fa: F[A]): A
+  def apply[A](fa: F[A], idx: Int): A
+  def get[A](fa: F[A], idx: Int): Option[A]
   def length[A](fa: F[A]): Int
+  def toArray[A](fa: F[A]): Array[A]
 }
 
 object Indexable {
   def apply[F[_]: Indexable]: Indexable[F] = macro summon[Indexable[F]]
-
-  implicit val IndexableDenseVector: Indexable[DenseVector] =
-    new Indexable[DenseVector] {
-      def apply[A](idx: Int, fa: DenseVector[A]): A = fa.apply(idx)
-      def length[A](fa: DenseVector[A]): Int = fa.length
-    }
-
-  implicit val IndexableDVector: Indexable[DVector] =
-    new Indexable[DVector] {
-      def apply[A](idx: Int, fa: DVector[A]): A = fa.apply(idx)
-      def length[A](fa: DVector[A]): Int = fa.length
-    }
-
 }
